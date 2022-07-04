77c3f76605eb7469018f4be763d40fe1bac4b7d8
Bouloche <v.mabille@rubika-edu.com>
2022-07-05 01:27:51 +0200

    Unit class & scriptables

--- MobileGaming/Assets/Scripts/GameLogic/Unit/Unit.cs
+++ MobileGaming/Assets/Scripts/GameLogic/Unit/Unit.cs
@@ -1,18 +1,35 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Mirror;
+using NaughtyAttributes;
 
-public class Unit : MonoBehaviour
+public class Unit : NetworkBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public string unitName;
+    public string faction;
+    public string className;
+    [SyncVar] public sbyte playerId;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    [ReadOnly] public sbyte baseMaxHp;
+    [ReadOnly] public sbyte basePhysicDef;
+    [ReadOnly] public sbyte baseMagicDef;
+    [ReadOnly] public sbyte baseAtkPerTurn;
+    [ReadOnly] public sbyte baseDamage;
+    [ReadOnly] public sbyte baseRange;
+    [ReadOnly] public sbyte baseMove;
+
+    [SyncVar] public sbyte MaxHp;
+    [SyncVar] public sbyte PhysicDef;
+    [SyncVar] public sbyte MagicDef;
+    [SyncVar] public sbyte AtkPerTurn;
+    [SyncVar] public sbyte Damage;
+    [SyncVar] public sbyte Range;
+    [SyncVar] public sbyte Move;
+
+    public NetworkAnimator Animator;
+    public ScriptableUnit unitScriptable;
+    public ScriptableAbility abilityScriptable;
+    public ScriptableAbility damageModifier;
+    public Outline outlineScript;
 }
