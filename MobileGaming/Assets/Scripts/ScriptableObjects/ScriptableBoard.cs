35103bc4fc682d3dbbf1e13079b73d01368d5d65
Super Hubert <h.ramiandrasoa@rubika-edu.com>
2022-07-05 01:30:48 +0200

    Created Tile script

--- MobileGaming/Assets/Scripts/ScriptableObjects/ScriptableBoard.cs
+++ MobileGaming/Assets/Scripts/ScriptableObjects/ScriptableBoard.cs
@@ -1,10 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using Tiles;
 
 [CreateAssetMenu(menuName = "Scriptables/Board")]
 public class ScriptableBoard : ScriptableObject
 {
-    public BaseTile[,] tiles = new BaseTile[10,10];
+    
 }
