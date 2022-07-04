7a6b9a12dc3f6e3e423138712901088bec790242
Super Hubert <h.ramiandrasoa@rubika-edu.com>
2022-07-05 01:42:47 +0200

    edited Tiles

--- MobileGaming/Assets/Scripts/GameLogic/Tile/Tile.cs
+++ MobileGaming/Assets/Scripts/GameLogic/Tile/Tile.cs
@@ -4,15 +4,9 @@ using UnityEngine;
 
 public class Tile : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public Unit currentUnit;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    public Vector3 coordinates;
+
+    public sbyte movementCost;
 }
