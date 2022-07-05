d2cfb0c6ff3b9e5112e71e0e03ea7c8936f2326a
Super Hubert <h.ramiandrasoa@rubika-edu.com>
2022-07-05 20:26:37 +0200

    Camera centers on grid creation

--- MobileGaming/Assets/Scripts/Map/HexGrid.cs
+++ MobileGaming/Assets/Scripts/Map/HexGrid.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using NaughtyAttributes;
@@ -6,10 +7,10 @@ using UnityEngine;
 public class HexGrid : MonoBehaviour
 {
     public Dictionary<Vector3Int,Hex> hexes = new ();
-
     public Vector2Int mapSize = new (8,10);
-    public float hexSize;
     public GameObject hexPrefab;
+
+    private Transform camAnchor;
     private static Vector3Int[] directionOffsets = new[]
     {
         new Vector3Int(1, 0, -1),
@@ -20,15 +21,21 @@ public class HexGrid : MonoBehaviour
         new Vector3Int(0, 1, -1)
     };
 
+    private void Start()
+    {
+        camAnchor = Camera.main.transform.parent;
+    }
+
     [Button]
     public void InstantiateHexes()
     {
+        DestroyPreviousGrid();
         for (sbyte x = 0; x < mapSize.x; x++)
         {
             for (sbyte y = 0; y < mapSize.y; y++)
             {
                 var xPos = y % 2 == 0 ? 2 * x : 2 * x + 1;
-                var hexGameObject = Instantiate(hexPrefab, new Vector3(xPos, 0, -1.73f * y), Quaternion.identity);
+                var hexGameObject = Instantiate(hexPrefab, new Vector3(xPos, 0, -1.73f * y), Quaternion.identity,transform);
                 hexGameObject.name = $"Hex {x},{y}";
                 var hex = hexGameObject.GetComponent<Hex>();
                 hex.col = x;
@@ -40,6 +47,16 @@ public class HexGrid : MonoBehaviour
         }
         
         UpdateNeighbours();
+        CenterCamera();
+    }
+
+    private void DestroyPreviousGrid()
+    {
+        hexes.Clear();
+        for (var i = transform.childCount - 1; i >= 0; i--)
+        {
+            Destroy(transform.GetChild(i).gameObject);
+        }
     }
 
     private void UpdateNeighbours()
@@ -56,6 +73,27 @@ public class HexGrid : MonoBehaviour
                 }
             }
         }
+    }
+
+    private void CenterCamera()
+    {
+        sbyte maxRow = 0;
+        sbyte minRow = 0;
+        sbyte maxCol = 0;
+        sbyte minCol = 0;
+        foreach (var hex in hexes.Values)
+        {
+            if (hex.row > maxRow) maxRow = hex.row;
+            if (hex.row < minRow) minRow = hex.row;
+            if (hex.col > maxCol) maxCol = hex.col;
+            if (hex.col < minCol) minCol = hex.col;
+        }
+
+        Debug.Log($"Highest row is {maxRow} and Lowest row is {minRow} \n Highest column is {maxCol} and Lowest column is {minCol}");
+        var yPos = (maxRow + minRow) / 2f;
+        var xPos = (maxCol + minCol) / 2f;
+
+        camAnchor.position = new Vector3(2 * xPos, 0,-yPos * 1.74f);
     }
 
     public Hex[] GetNeighbours(Hex hex)
